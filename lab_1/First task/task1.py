ec5b2f8690b6c302a148ba334822a6875451cbd1
Михаил Насонов <mnasonovy@gmail.com>
2024-04-02 19:37:22 +0400

    writing an encryption function for a given text using a given key

--- lab_1/First task/task1.py
+++ lab_1/First task/task1.py
@@ -3,19 +3,25 @@ import os
 
 
 def generate_cipher_key(shift, filename):
-    # Генерация ключа шифрования для алфавита с учетом сдвига
     alphabet = list('абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
     shifted_alphabet = alphabet[shift % len(alphabet):] + alphabet[:shift % len(alphabet)]
     cipher_key = dict(zip(alphabet, shifted_alphabet))
-    
-    # Сохранение ключа в JSON файл
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(cipher_key, file, ensure_ascii=False)
-    
-    # Возвращаем ключ
     return {'original_alphabet': alphabet, 'shifted_alphabet': shifted_alphabet}
 
+def encrypt_text(input_file, output_file, cipher_key_file):
+    with open(input_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+    with open(cipher_key_file, 'r', encoding='utf-8') as file:
+        cipher_key = json.load(file)
+    encrypted_text = ''
+    for char in text:
+        encrypted_text += cipher_key.get(char, char)
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(encrypted_text)
+        
 with open(os.path.join('First task', 'settings.json'), 'r') as settings:
         settings = json.load(settings)
 cipher_key = generate_cipher_key(settings['shift'], settings['random_key'])
-
+encrypt_text(settings['sourse_text'], settings['encrypted text'], settings['random_key'])
