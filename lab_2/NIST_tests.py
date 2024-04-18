89d9567e7a8079d69379aa13219157f26ad8832c
Михаил Насонов <mnasonovy@gmail.com>
2024-04-18 21:33:40 +0400

    writing a Frequency bitwise test

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -1,3 +1,5 @@
+import math
+
 def read_sequence_from_file(filename: str) -> str:
 
     with open(filename, 'r') as file:
@@ -8,5 +10,20 @@ def write_results_to_file(filename: str, results: dict):
     with open(filename, 'w') as file:
         for test, result in results.items():
             file.write(f"{test}: {result}\n")
+            
+def frequency_bitwise_test(sequence: str) -> float:
+    """
+    Calculate the P value for the frequency bitwise test of a sequence.
 
+    Parameters:
+        sequence (str): Sequence of bytes.
+
+    Returns:
+        float: The P value.
+    """
+    N = len(sequence)
+    sum_bits = sum(1 if bit == '1' else -1 for bit in sequence)
+    P = sum_bits / math.sqrt(N)
+    P = math.erfc(P / math.sqrt(2))
+    return P
 
